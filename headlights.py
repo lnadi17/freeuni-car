d375052165541f78ca715f58db8167c71978b8bd
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2019-07-17 03:21:41 +0400

    Correct syntax errors & add try/catch in find.py

--- headlights.py
+++ headlights.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(7, GPIO.OUT)
 
-def update_headlight(data):
+def update_headlights(data):
 	data = data.decode('utf-8').lower()
 	index = data.indexOf(' ')
 	first_word = data.substring(0, index)
 
 	if ("headlights" == first_word):
-		if ("true" in data.substring(index))
+		if ("true" in data.substring(index)):
 			GPIO.output(7, GPIO.HIGH)
-		else if ("false" in data.substring(index)):
+		elif ("false" in data.substring(index)):
 			GPIO.output(7, GPIO.LOW)
