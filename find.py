ec6cde8ec4f49d7a07039a550d32b777b028ee9b
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2019-07-16 17:56:56 +0400

    Update find.py

--- find.py
+++ find.py
@@ -4,7 +4,7 @@ from threading import Event
 
 def update_location(event, connection):
 	while (event.is_set()):
-		result = subprocess.run(['sudo', '/home/pi/Desktop/webcar/fingerprint', '-g', 'test', '-s', 'http://192.168.98.17:8003', '-c', '1', '--nodebug'], stdout=subprocess.PIPE)
+		result = subprocess.run(['sudo', '/home/pi/Desktop/webcar/fingerprint', '-g', 'eng', '-s', 'http://192.168.98.17:8003', '-c', '1', '--nodebug'], stdout=subprocess.PIPE)
 		if (result):
 			connection.sendall(b'location ' + result.stdout)
 		sleep(1)
