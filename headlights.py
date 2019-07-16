c445fccbaede970ed4db31f90b53828928c41322
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2019-07-17 03:07:30 +0400

    Update headlights.py

--- headlights.py
+++ headlights.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(7,GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 
 def update_headlight(data):
 	data = data.decode('utf-8').lower()
 	index = data.indexOf(' ')
-	first word = data.substring(0, index)
+	first_word = data.substring(0, index)
 
-	if("headlights" == first word):
-		if("true" in data.substring(index))
-		GPIO.output(7, GPIO.HIGH)
-
-		else if("false" in data.substring(index)):
+	if ("headlights" == first_word):
+		if ("true" in data.substring(index))
+			GPIO.output(7, GPIO.HIGH)
+		else if ("false" in data.substring(index)):
 			GPIO.output(7, GPIO.LOW)
