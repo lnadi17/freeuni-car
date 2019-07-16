4f8efbe92eb717654e11bbbe4b4c3b9f4b1a9e6f
giorgisaatashvili <52978775+giorgisaatashvili@users.noreply.github.com>
2019-07-17 03:02:31 +0400

    update to headlights.py

--- headlights.py
+++ headlights.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(7,GPIO.OUT)
 
 def update_headlight(data):
-	GPIO.setup(7,GPIO.OUT)
+	data = data.decode('utf-8').lower()
+	index = data.indexOf(' ')
+	first word = data.substring(0, index)
 
-	if(data == "dark"):
+	if("headlights" == first word):
+		if("true" in data.substring(index))
 		GPIO.output(7, GPIO.HIGH)
 
-	else if(data == "bright"):
-		GPIO.output(7, GPIO.LOW)
+		else if("false" in data.substring(index)):
+			GPIO.output(7, GPIO.LOW)
